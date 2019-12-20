716fa414e98f788cee124c4ed6c8ba3000dd0cd7
josh <joshbothun@gmail.com>
2019-12-20 05:30:49 -0800

    Add missing debug UI Script

---
+++ Assets/Scripts/Debug/DebugUI.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugUI : MonoBehaviour {
+  public Text debugText;
+
+  private Dictionary<string, object> debugValues;
+
+  private void Awake() {
+    DontDestroyOnLoad(gameObject);
+
+    debugValues = new Dictionary<string, object>();
+    Refresh();
+  }
+
+  public void ShowValueInternal(string key, object val) {
+    debugValues[key] = val;
+    Refresh();
+  }
+
+  public void HideValueInternal(string key) {
+    if (debugValues.Remove(key)) {
+      Refresh();
+    }
+  }
+
+  public static void ShowValue(string key, object val) {
+    Instance.ShowValueInternal(key, val);
+  }
+
+  public static void HideValue(string key) {
+    Instance.HideValueInternal(key);
+  }
+
+  private void Refresh() {
+    StringBuilder builder = new StringBuilder();
+    foreach (var item in debugValues) {
+      builder.AppendLine($"[{item.Key}] {item.Value}");
+    }
+    debugText.text = builder.ToString();
+  }
+
+  // Singleton access.
+  private static DebugUI _instance;
+  private static DebugUI Instance {
+    get {
+      if (_instance == null) {
+        _instance = GameObject.FindObjectOfType<DebugUI>();
+      }
+      return _instance;
+    }
+  }
+}
