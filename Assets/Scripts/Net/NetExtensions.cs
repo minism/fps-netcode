62880cacd229d7a2e162ccb06247cdd6fe246d06
josh <joshbothun@gmail.com>
2020-05-16 16:51:52 -0700

    Compress quaternions to reduce bandwidth further.

--- Assets/Scripts/Net/NetExtensions.cs
+++ Assets/Scripts/Net/NetExtensions.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 /// Extensions to the LiteNetLib NetDataReader/NetDataWriter interface for other types.
 public static class NetExtensions {
+  private const float QUAT_FLOAT_PRECISION_MULT = 10000f;
+
   public static void SerializeVector3(NetDataWriter writer, Vector3 vector) {
     writer.Put(vector.x);
     writer.Put(vector.y);
@@ -18,19 +20,79 @@ public static class NetExtensions {
   }
 
   public static void SerializeQuaternion(NetDataWriter writer, Quaternion quaternion) {
-    writer.Put(quaternion.w);
-    writer.Put(quaternion.x);
-    writer.Put(quaternion.y);
-    writer.Put(quaternion.z);
+    // Utilize "Smallest three" strategy.
+    // Reference: https://gafferongames.com/post/snapshot_compression/
+    byte maxIndex = 0;
+    float maxValue = float.MinValue;
+    float maxValueSign = 1;
+
+    // Find the largest value in the quaternion and save its index.
+    for (byte i = 0; i < 4; i++) {
+      var value = quaternion[i];
+      var absValue = Mathf.Abs(value);
+      if (absValue > maxValue) {
+        maxIndex = i;
+        maxValue = absValue;
+
+        // Note the sign of the maxValue for later.
+        maxValueSign = Mathf.Sign(value);
+      }
+    }
+
+    // Encode the smallest three components.
+    short a, b, c;
+    switch (maxIndex) {
+      case 0:
+        a = (short)(quaternion.y * maxValueSign * QUAT_FLOAT_PRECISION_MULT);
+        b = (short)(quaternion.z * maxValueSign * QUAT_FLOAT_PRECISION_MULT);
+        c = (short)(quaternion.w * maxValueSign * QUAT_FLOAT_PRECISION_MULT);
+        break;
+      case 1:
+        a = (short)(quaternion.x * maxValueSign * QUAT_FLOAT_PRECISION_MULT);
+        b = (short)(quaternion.z * maxValueSign * QUAT_FLOAT_PRECISION_MULT);
+        c = (short)(quaternion.w * maxValueSign * QUAT_FLOAT_PRECISION_MULT);
+        break;
+      case 2:
+        a = (short)(quaternion.x * maxValueSign * QUAT_FLOAT_PRECISION_MULT);
+        b = (short)(quaternion.y * maxValueSign * QUAT_FLOAT_PRECISION_MULT);
+        c = (short)(quaternion.w * maxValueSign * QUAT_FLOAT_PRECISION_MULT);
+        break;
+      case 3:
+        a = (short)(quaternion.x * maxValueSign * QUAT_FLOAT_PRECISION_MULT);
+        b = (short)(quaternion.y * maxValueSign * QUAT_FLOAT_PRECISION_MULT);
+        c = (short)(quaternion.z * maxValueSign * QUAT_FLOAT_PRECISION_MULT);
+        break;
+      default:
+        throw new System.InvalidProgramException("Unexpected quaternion index.");
+    }
+
+    writer.Put(maxIndex);
+    writer.Put(a);
+    writer.Put(b);
+    writer.Put(c);
   }
 
   public static Quaternion DeserializeQuaternion(NetDataReader reader) {
-    Quaternion q;
-    q.w = reader.GetFloat();
-    q.x = reader.GetFloat();
-    q.y = reader.GetFloat();
-    q.z = reader.GetFloat();
-    return q;
+    // Read values from the wire and map back to normal float precision.
+    byte maxIndex = reader.GetByte();
+    float a = reader.GetShort() / QUAT_FLOAT_PRECISION_MULT;
+    float b = reader.GetShort() / QUAT_FLOAT_PRECISION_MULT;
+    float c = reader.GetShort() / QUAT_FLOAT_PRECISION_MULT;
+
+    // Reconstruct the fourth value.
+    float d = Mathf.Sqrt(1f - (a * a + b * b + c * c));
+    switch (maxIndex) {
+      case 0:
+        return new Quaternion(d, a, b, c);
+      case 1:
+        return new Quaternion(a, d, b, c);
+      case 2:
+        return new Quaternion(a, b, d, c);
+      case 3:
+        return new Quaternion(a, b, c, d);
+      default:
+        throw new System.InvalidProgramException("Unexpected quaternion index.");
+    }
   }
 
   public static void Put(this NetDataWriter writer, Quaternion quaternion) {
